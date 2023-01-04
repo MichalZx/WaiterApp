450d4f17b9df5ebe0d90b128647681ca4526c8e0
JaFi01 <116461510+JaFi01@users.noreply.github.com>
2023-01-04 02:16:09 +0100

    Wyświetlanie rachunków na rozwijalnej liście, zamykanie rachunków (wymaga dopracowania)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
+using System.Diagnostics;
 
 namespace WaiterApp
 {
@@ -41,6 +42,11 @@ namespace WaiterApp
         }
 
     }
+    public class FileItem
+    {
+        public string Name { get; set; }
+        public string FullPath { get; set; }
+    }
     public partial class MainWindow : Window
     {
         public int iloscrachunkow = 0;
@@ -49,115 +55,54 @@ namespace WaiterApp
         public MainWindow()
         {
             InitializeComponent();
-            tbZamowienie1.Visibility = Visibility.Hidden;
-            tbZamowienie2.Visibility = Visibility.Hidden;
-            tbZamowienie3.Visibility = Visibility.Hidden;
-            tbZamowienie4.Visibility = Visibility.Hidden;
 
-            string folderPath = @"..\..\..\Rachunki\";
-            List<string> aktualneRachunki=new List<string>() ;
-            foreach (string file in Directory.EnumerateFiles(folderPath, "*.txt"))
+            string closedFilePath = @"..\..\..\ZamknieteRachunki\";
+            if (!Directory.Exists(closedFilePath))
             {
-                aktualneRachunki.Add(File.ReadAllText(file));
+                Directory.CreateDirectory(closedFilePath);
             }
 
-
-            if (aktualneRachunki.Count>=1)
-            {
-                tbZamowienie1.Visibility = Visibility.Visible;
-                tbZamowienie1.Text = aktualneRachunki[0];
-            }
-            if(aktualneRachunki.Count>=2)
-            {
-                tbZamowienie2.Visibility = Visibility.Visible;
-                tbZamowienie2.Text = aktualneRachunki[1];
-            }
-            if(aktualneRachunki.Count>=3)
-            {
-                tbZamowienie3.Visibility = Visibility.Visible;
-                tbZamowienie3.Text = aktualneRachunki[2];
-            }
-            if(aktualneRachunki.Count>=4)
-            {
-                tbZamowienie4.Visibility = Visibility.Visible;
-                tbZamowienie4.Text = aktualneRachunki[3];
-            }
 
         }
         #endregion
-
-        #region bezsensowny Main z użytecznymi metodami
-        //Cały ten przeciążony main jest 
-        public MainWindow(int numerStolika, int iloscGosci, bool prio, List<Danie> zamowienie, string kelnerID, DateTime czasotwarcia)
+        private void ComboBox_Loaded(object sender, RoutedEventArgs e)
         {
-            InitializeComponent();
-            tbZamowienie1.Visibility = Visibility.Hidden;
-            tbZamowienie2.Visibility = Visibility.Hidden;
-            tbZamowienie3.Visibility = Visibility.Hidden;
-            tbZamowienie4.Visibility = Visibility.Hidden;
-
-            bool juzDodano = false; //gdyby nie ta zmienna to wszystkie rachunki wyglądałyby tak samo
-
-            if (tbZamowienie1.Text == "")
+            string folderPath = @"..\..\..\Rachunki\"; // ścieżka do folderu z plikami .txt
+            string[] txtFiles = Directory.GetFiles(folderPath, "*.txt");
+            foreach (string file in txtFiles)
             {
-                //gridTBZamowienie1.Margin = new Thickness(20, 20, 405, 310);
-                tbZamowienie1.Visibility = Visibility.Visible;
-                //tbZamowienie1.Visibility.visi
-                tbZamowienie1.Text = $"Nr stolika: {numerStolika,-4} Ilość gości: {iloscGosci,-4} {(prio ? "Priorytetowe" : "Zwykłe"),-20} {kelnerID} {czasotwarcia}\n\n";
-                foreach (Danie item in zamowienie)
-                {
-                    tbZamowienie1.Text += item.ToString() + "\n";
-                    tbZamowienie1.Height += 25;
-                  //  gridTBZamowienie1.Margin = new Thickness(20, 20, 405, gridTBZamowienie1.Margin.Bottom - 25);
-                }
-                juzDodano = true;
-                btZamknijZamowienie1.Visibility = Visibility.Visible;
-                
+                FileItem item = new FileItem();
+                item.Name = System.IO.Path.GetFileName(file);
+                item.FullPath = file;
+                cbFiles.Items.Add(item);
             }
-
-            if (tbZamowienie2.Text == "" & juzDodano == false)
-            {
-                tbZamowienie2.Visibility = Visibility.Visible;
-                tbZamowienie2.Margin = new Thickness(400, 20, 0, 0);
-                tbZamowienie2.Text = $"Nr stolika: {numerStolika,-4} Ilość gości: {iloscGosci,-4} {(prio ? "Priorytetowe" : "Zwykłe"),-20}  {kelnerID} {czasotwarcia} \n\n";
-                foreach (Danie item in zamowienie)
-                {
-                    tbZamowienie2.Text += item.ToString() + "\n";
-                    tbZamowienie2.Height += 30;
-                }
-                juzDodano = true;
-            }
-            if (tbZamowienie3.Text == "" & juzDodano == false)
-            {
-                tbZamowienie3.Visibility = Visibility.Visible;
-
-                tbZamowienie3.Margin = new Thickness(20, tbZamowienie1.Height, 0, 0);
-                tbZamowienie3.Text = $"Nr stolika: {numerStolika,-4} Ilość gości: {iloscGosci,-4} {(prio ? "Priorytetowe" : "Zwykłe"),-20} \n\n";
-                foreach (Danie item in zamowienie)
-                {
-                    tbZamowienie3.Text += item.ToString() + "\n";
-                    tbZamowienie3.Height += 30;
-                }
-                juzDodano = true;
-            }
-            if (tbZamowienie4.Text != "" & juzDodano == false)
-            {
-                tbZamowienie4.Visibility = Visibility.Visible;
-
-                tbZamowienie4.Margin = new Thickness(20, tbZamowienie2.Height, 0, 0);
-                tbZamowienie4.Text = $"Nr stolika: {numerStolika,-4} Ilość gości: {iloscGosci,-4} {(prio ? "Priorytetowe" : "Zwykłe"),-20} \n\n";
-                foreach (Danie item in zamowienie)
-                {
-                    tbZamowienie4.Text += item.ToString() + "\n";
-                    tbZamowienie4.Height += 30;
-                }
-                juzDodano = true;
-            }
-
         }
-        #endregion
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            FileItem selectedItem = (FileItem)cbFiles.SelectedItem;
+            if (selectedItem != null)
+            {
+                string content = File.ReadAllText(selectedItem.FullPath);
+                tbContent.Text = content;
+            }
+        }
+
+        private void MoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileItem selectedItem = (FileItem)cbFiles.SelectedItem;
+            if (selectedItem != null)
+            {
+                string sourcePath = selectedItem.FullPath;
+                string targetPath = @"..\..\..\ZamknieteRachunki\" + selectedItem.Name;
+
+                File.Move(sourcePath, targetPath);
+            }
+        }
+
+
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
         {
             string idKelnera = tbNazwaKelnera.Text;
             KelnerzyDane p = new KelnerzyDane();
@@ -174,17 +119,14 @@ namespace WaiterApp
             }
             przycisk.Opacity = 0;
         }
-        private void btZamknijZamowienie_Click(object sender, RoutedEventArgs e)
-        {
-            tbZamowienie1.Text = "";
-        }
+
 
         private void tempPOkaRachunek_Click(object sender, RoutedEventArgs e)
         {
             string path= @"..\..\..\Rachunki\17122022_202854bill.txt";      // to jest przykladowy plik zeby zobaczyc czy dziala
             SzczegolyRachunku info = new SzczegolyRachunku(path);           // te 3 kolejne linijki sa dobrze i nic wiecej nie trezba
             info.WyswietlSzczegolyRachunku();
-            rachunkiInfo.Content = info.lb_info.Content;
+            //rachunkiInfo.Content = info.lb_info.Content;
         }
 
     }
